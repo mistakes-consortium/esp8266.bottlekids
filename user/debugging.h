e0ade5a06a8ce755da3214cdbc6b062dcf03814b
Eric Stein <toba@des.truct.org>
2015-07-11 19:31:04 -0500

    asyle of debugging.h

--- user/debugging.h
+++ user/debugging.h
@@ -11,48 +11,48 @@ char debugging_str[255];
 #endif
 
 void print_hexdump(char* data, size_t bytes) {
-	DEBUGUART("%d bytes:\r\n", bytes);
+    DEBUGUART("%d bytes:\r\n", bytes);
 
-	uint16_t row;
-	uint8_t per_row = 16;
-	uint16_t i;
-	int mode = 0;
-	int printed;
-	for (row = 0; row <= (bytes / per_row); row++) {
-		uint16_t data_this_row = min((bytes - (row * per_row)), per_row);
-		//DEBUGUART("data entries this row: %d\r\n", data_this_row);
+    uint16_t row;
+    uint8_t per_row = 16;
+    uint16_t i;
+    int mode = 0;
+    int printed;
+    for (row = 0; row <= (bytes / per_row); row++) {
+        uint16_t data_this_row = min((bytes - (row * per_row)), per_row);
+        //DEBUGUART("data entries this row: %d\r\n", data_this_row);
 
-		for (mode = 0; mode < 2; mode++) {
-			printed = 0;
-			for (i = 0; i < data_this_row; i++) {
-				unsigned char c = data[row * per_row + i];
-				if (mode == 0) {
-					if ((c >= 0x20) && (c <= 0x7e)) {
-						DEBUGUART("%c", c);
-					} else {
-						DEBUGUART(".", c)
-					}
-				} else {
-					printed++;
-					DEBUGUART("%x%x", (c >> 4), (c % 0x10));
-				}
-			}
-			//DEBUGUART("\r\n I printed %d bytes of hex.\r\n", printed);
+        for (mode = 0; mode < 2; mode++) {
+            printed = 0;
+            for (i = 0; i < data_this_row; i++) {
+                unsigned char c = data[row * per_row + i];
+                if (mode == 0) {
+                    if ((c >= 0x20) && (c <= 0x7e)) {
+                        DEBUGUART("%c", c);
+                    } else {
+                        DEBUGUART(".", c)
+                    }
+                } else {
+                    printed++;
+                    DEBUGUART("%x%x", (c >> 4), (c % 0x10));
+                }
+            }
+            //DEBUGUART("\r\n I printed %d bytes of hex.\r\n", printed);
 
-			for (; i < per_row; i++) {
-				if (mode == 0) {
-					DEBUGUART("%s", " ");
-				} else {
-					DEBUGUART("%s", "   ");
-				}
-			}
+            for (; i < per_row; i++) {
+                if (mode == 0) {
+                    DEBUGUART("%s", " ");
+                } else {
+                    DEBUGUART("%s", "   ");
+                }
+            }
 
-			if (mode == 0) {
-				DEBUGUART("%s", "  ");
-			}
-		}
-		DEBUGUART("%s", "\r\n");
-	}
+            if (mode == 0) {
+                DEBUGUART("%s", "  ");
+            }
+        }
+        DEBUGUART("%s", "\r\n");
+    }
 }
 
 #endif
